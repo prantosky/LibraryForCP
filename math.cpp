e06993e50653d841664c65cb1f86c7dbbf717b9c
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2021-03-03 18:24:38 +0530

    Implement functions in prime namespace

--- math.cpp
+++ math.cpp
@@ -1,26 +1,23 @@
+#include <cmath>
 #include <iostream>
-#include <stdexcept>
+#include <iterator>
 #include <type_traits>
+#include <vector>
 
-namespace Math {
+namespace math {
+
 	template <typename T,
 			  std::enable_if_t<std::is_integral<T>::value, bool> = true>
-	long pow_mod(T radix, int exponent, const T mod) {
-		if (exponent < 0) {
-			throw std::invalid_argument(
-				"math::pow_mod: pow_mod will misbehave for negative values of "
-				"exponent");
-		}
-		long base = radix;
+	long pow_mod(long radix, unsigned long exponent, const long mod) {
 		long result = 1;
 
 		while (exponent > 0) {
 			if ((exponent & 1) != 0) {
-				result = (result * base) % mod;
+				result = (result * radix) % mod;
 			}
 			exponent >>= 1;
 			if (exponent == 0) break;
-			base = (base * base) % mod;
+			radix = (radix * radix) % mod;
 		}
 		return result;
 	}
@@ -28,33 +25,113 @@ namespace Math {
 	template <typename T,
 			  std::enable_if_t<std::is_integral<T>::value, bool> = true>
 	long pow(T radix, int exponent) {
-		long base = radix;
 		long result = 1;
 		while (exponent != 0) {
 			if ((exponent & 1) != 0) {
-				result *= base;
+				result *= radix;
 			}
 			exponent >>= 1;
 			if (exponent == 0) break;
-			base *= base;
+			radix *= radix;
 		}
 		return result;
 	}
 
 	template <typename T,
 			  std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
-	double pow(T radix, int exponent) {
-		double base = radix;
+	double pow(double radix, int exponent) {
 		double result = 1;
 		while (exponent != 0) {
 			if ((exponent & 1) != 0) {
-				result *= base;
+				result *= radix;
 			}
 			exponent >>= 1;
 			if (exponent == 0) break;
-			base *= base;
+			radix *= radix;
 		}
 		return result;
 	}
+}  // namespace math
 
-}  // namespace Math
+namespace prime {
+
+	std::vector<int> first_n_primes(const size_t n) {
+		if (n == 0) return {};
+		if (n == 1) return {2};
+
+		std::vector<int> primes({2, 3});
+		primes.reserve(n);
+
+		size_t i = 2;
+		size_t candidate = 5;
+		bool is_prime = false;
+
+		while (i < n) {
+			is_prime = true;
+			long sqrt = std::ceil(std::sqrt(candidate));
+
+			for (auto& prime : primes) {
+				if (prime > sqrt) break;
+				if (candidate % prime == 0) {
+					is_prime = false;
+				}
+			}
+			if (is_prime) {
+				primes.emplace_back(candidate);
+				i++;
+			}
+			candidate += 2;
+		}
+		return primes;
+	}
+
+	void generate_next_primes(std::vector<int>& primes,
+							  const size_t count = 2) {
+		if (primes.empty()) {
+			primes = prime::first_n_primes(count);
+			return;
+		} else if (primes.size() == 1) {
+			primes = prime::first_n_primes(count - 1);
+			return;
+		}
+
+		size_t i = 0;
+		size_t candidate = *primes.rbegin() + 2;
+		bool is_prime = false;
+
+		while (i < count) {
+			is_prime = true;
+			long sqrt = std::ceil(std::sqrt(candidate));
+
+			for (auto& prime : primes) {
+				if (prime > sqrt) break;
+				if (candidate % prime == 0) {
+					is_prime = false;
+				}
+			}
+			if (is_prime) {
+				primes.emplace_back(candidate);
+				i++;
+			}
+			candidate += 2;
+		}
+	}
+
+	bool is_prime(std::vector<int>& primes, const int num, const int step = 2) {
+		if (primes.empty()) {
+			primes = prime::first_n_primes(step);
+		}
+		long sqrt = std::ceil(std::sqrt(num));
+		while (sqrt > *primes.rbegin()) {
+			prime::generate_next_primes(primes, step);
+		}
+		for (auto& prime : primes) {
+			if (prime > sqrt) break;
+			if (num % prime == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}  // namespace prime
