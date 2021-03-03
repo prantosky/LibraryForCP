f87506214a92d874f73299627608a23da9c24e06
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2021-03-03 13:45:00 +0530

    Add string_helper namespace

--- string_helper.cpp
+++ string_helper.cpp
@@ -3,30 +3,25 @@
 #include <sstream>
 #include <vector>
 
-class StringHelper {
- public:
-	static std::vector<std::string> split(const std::string& str,
-										  const char separator = ' ');
-	static std::vector<std::string> regexSplit(const std::string& str,
-											   const std::regex& pattern);
-};
-
-std::vector<std::string> StringHelper::split(const std::string& str,
-											 const char separator) {
-	std::vector<std::string> results;
-	std::stringstream iss(str);
-	std::string token;
-	while (std::getline(iss, token, separator)) {
-		if (not token.empty()) results.push_back(token);
-		token = "";
+namespace string_helper {
+	std::vector<std::string> split(const std::string& str,
+								   const char separator) {
+		std::vector<std::string> results;
+		std::stringstream iss(str);
+		std::string token;
+		while (std::getline(iss, token, separator)) {
+			if (not token.empty()) results.push_back(token);
+			token = "";
+		}
+		return results;
 	}
-	return results;
-}
 
-std::vector<std::string> StringHelper::regexSplit(const std::string& str,
-												  const std::regex& pattern) {
-	std::vector<std::string> results;
-	std::copy(std::sregex_token_iterator(str.begin(), str.end(), pattern, -1),
-			  std::sregex_token_iterator(), std::back_inserter(results));
-	return results;
-}
+	std::vector<std::string> regexSplit(const std::string& str,
+										const std::regex& pattern) {
+		std::vector<std::string> results;
+		std::copy(
+			std::sregex_token_iterator(str.begin(), str.end(), pattern, -1),
+			std::sregex_token_iterator(), std::back_inserter(results));
+		return results;
+	}
+}  // namespace string_helper
